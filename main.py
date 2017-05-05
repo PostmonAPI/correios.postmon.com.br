fecac540cc3db8714daaf87da5032efeec094a5f
Iuri de Silvio <iurisilvio@gmail.com>
2017-05-05 13:22:20 -0300

    Tela de rastreamento standalone.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from __future__ import absolute_import
 
 from flask import Flask
 
-from postmon_correios import webservice, websro
+from postmon_correios import rastreamento, webservice, websro
 
 app = Flask(__name__)
 webservice.init_app(app, url_prefix='/webservice')
 websro.init_app(app, url_prefix='/websro')
+rastreamento.init_app(app, url_prefix='/rastreamento')
--- postmon_correios/rastreamento.py
+++ postmon_correios/rastreamento.py
@@ -3,21 +3,29 @@ from __future__ import absolute_import
 
 from flask import Blueprint, render_template, request
 
-bp = Blueprint('websro', __name__)
-
-
 from .webservice import correios
+
+bp = Blueprint('rastreamento', __name__)
 
 
 @bp.route("/")
-def index():
+def x():
     keys = ["P_COD_UNI", "objetos", "objeto"]
     for key in keys:
         objetos = request.args.get(key)
         if objetos is not None:
             break
     response = correios.buscaEventos(objetos)
-    return render_template('websro.html', objeto=response["objeto"][0])
+    objeto = response["objeto"][0]
+
+    if objeto.get("evento"):
+        template = 'rastreamento.html'
+        status_code = 200
+    else:
+        template = 'rastreamento_nao_encontrado.html',
+        status_code = 404
+
+    return render_template(template, objeto=objeto), status_code
 
 
 def init_app(app, url_prefix=None):
